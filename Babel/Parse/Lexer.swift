157321480ea7290fac18d2ee5d3fb611ff08b24a
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2016-01-21 01:21:52 -0800

    Use clang’s lexer (via ClangKit)

--- Babel/Parse/Lexer.swift
+++ Babel/Parse/Lexer.swift
@@ -11,17 +11,23 @@ import Foundation
 class Lexer {
     
     let source: String
+    let tokens: [CKToken]
+    var tokenGenerator: IndexingGenerator<[CKToken]>
 
     init(source: String) {
         self.source = source
+        
+        let translationUnit = CKTranslationUnit(text: source, language: CKLanguageObjC, args: [])
+        self.tokens = translationUnit.tokens as! [CKToken]
+        self.tokenGenerator = self.tokens.generate()
     }
     
     func findNext() -> Token? {
-        guard let ch = nextMeaningfulCharacter() else {
+        guard let ckToken = tokenGenerator.next() else {
             return nil
         }
         
-        print(ch)
+        print(ckToken)
         
         return Token()
     }
