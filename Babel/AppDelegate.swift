ee7f1d307f36329ac76dc0d068014e116c78c9fb
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2016-01-21 01:08:38 -0800

    Initial commit

---
+++ Babel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Babel
+//
+//  Created by Dongyuan Liu on 2015-09-01.
+//  Copyright © 2015 Xhacker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Babel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Babel
+//
+//  Created by Dongyuan Liu on 2015-09-01.
+//  Copyright © 2015 Xhacker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
